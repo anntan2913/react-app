1f6d4af7cbab576b5830988c45091c633708c0fd
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-05-22 19:17:57 +0200

    add component Container

--- my-first-react-app/src/components/SearchForm/SearchForm.js
+++ my-first-react-app/src/components/SearchForm/SearchForm.js
@@ -5,7 +5,9 @@ const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search..." />            
-            <Button text="Search" />            
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
         </form>
     );
 };
