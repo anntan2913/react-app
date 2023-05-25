0412aa57f1dc2cbb5db537aa1f6f322ed097f0b6
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-05-25 22:02:39 +0200

    modify components: ColumnForm, Button, TextInput

--- my-first-react-app/src/components/ColumnForm/ColumnForm.js
+++ my-first-react-app/src/components/ColumnForm/ColumnForm.js
@@ -1,5 +1,7 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 
 const ColumnForm = props => {
 
@@ -14,10 +16,12 @@ const ColumnForm = props => {
     };
 
 	return (
-        <form onSubmit={handleSubmit}>
-           Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
-           Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
-            <button>Add column</button>
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+           <label>Title: </label>
+           <TextInput value={title} onChange={e => setTitle(e.target.value)} />
+           <label>Icon: </label>
+           <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
+           <Button>Add column</Button>
         </form>
 	);
 };
