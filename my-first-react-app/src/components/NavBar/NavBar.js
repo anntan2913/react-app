f5ea9dcc35b0e453c4b55329ad25ce371a616aa2
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-10 18:51:53 +0200

    add func Active Links: add component NavLink

--- my-first-react-app/src/components/NavBar/NavBar.js
+++ my-first-react-app/src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import Container from '../Container/Container';
 import styles from './NavBar.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -8,14 +9,14 @@ const NavBar = () => {
                 <div className={styles.navcontent}>
                     <a href="/"><i className='fa fa-tasks'/></a>
                     <ul className={styles.subpage}>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/favorite">Favorite</a></li>
-                        <li><a href="/about">About</a></li>
+                        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+                        <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
                     </ul>
                 </div>
             </Container>
         </nav>
-    )
-}
+    );
+};
 
 export default NavBar;
