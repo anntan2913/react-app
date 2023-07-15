17a8bad205d517cd772858f586ced5fda83b04d1
Anna Tankielun <anna.tankielun.3@gmail.com>
2023-07-15 12:11:51 +0200

    add new function: remove card

--- my-first-react-app/src/components/Card/Card.js
+++ my-first-react-app/src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import styles from './Card.module.scss';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 import clsx from 'clsx';
 
 const Card = props => {
@@ -12,11 +12,21 @@ const Card = props => {
         dispatch(toggleCardFavorite(props.id));
     };
 
+    const handleClickRemove = e => {
+        e.preventDefault();
+        dispatch(removeCard(props.id));
+    };
+
     return (
         <li className={styles.card}>{props.title}
+            <div className='style.buttons'>
             <button onClick={handleClick} className={clsx(styles.button, props.isFavorite && styles.active)}>
                 <i className="fa fa-star-o" />
             </button>
+            <button onClick={handleClickRemove} className={styles.button} >
+                <i className="fa fa-trash" />
+            </button>
+            </div>
         </li>
     );
 };
